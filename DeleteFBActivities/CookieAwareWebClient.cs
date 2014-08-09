b752c1e32a91a72bb7acfb2a2c00310795b09600
HabibAhmedQureshi <habib.ahmed@gmail.com>
2014-08-09 21:46:18 +0200

    Base

---
+++ DeleteFBActivities/CookieAwareWebClient.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net;
+
+namespace DeleteFBActivities
+{
+	class CookieAwareWebClient : WebClient
+	{
+		private CookieContainer cc = new CookieContainer();
+		private string lastPage;
+
+		protected override WebRequest GetWebRequest(System.Uri address)
+		{
+			WebRequest R = base.GetWebRequest(address);
+			if (R is HttpWebRequest)
+			{
+				HttpWebRequest WR = (HttpWebRequest)R;
+				AddCookies(WR);
+				if (lastPage != null)
+				{
+					WR.Referer = lastPage;
+				}
+			}
+			lastPage = address.ToString();
+			return R;
+		}
+
+        /// <summary>
+        /// From Fiddler:
+        /// GET https://www.facebook.com/the.habib.qureshi/allactivity HTTP/1.1
+        /// Accept: text/html, application/xhtml+xml, */*
+        /// Accept-Language: de-CH,de;q=0.8,en-GB;q=0.6,en;q=0.4,da;q=0.2
+        /// User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.3; WOW64; Trident/7.0; MAARJS)
+        /// Accept-Encoding: gzip, deflate
+        /// Host: www.facebook.com
+        /// DNT: 1
+        /// Connection: Keep-Alive
+        /// Cookie: datr=mpuHUltlM5M4tVf2i2GPTKu9; lu=TgUdlrddFNTjJuQ2_Md2haUw; locale=en_GB; c_user=100006494321954; fr=0IYyemryDVkPeh8IL.AWVx5Bt7tcKk-t91kfFF3tzTqkc.BT5nRM.0s.FPm.AWVdrvdp; xs=107%3Ahlw-ZENUDzjacA%3A2%3A1407611980%3A8886; csm=2; s=Aa7ySACnrs6HVvqp
+        /// -------------------Breaking down the cookie array---------------
+        /// datr=mpuHUltlM5M4tVf2i2GPTKu9;
+        /// lu=TgUdlrddFNTjJuQ2_Md2haUw;
+        /// locale=en_GB;
+        /// c_user=100006494321954;
+        /// fr=0IYyemryDVkPeh8IL.AWVx5Bt7tcKk-t91kfFF3tzTqkc.BT5nRM.0s.FPm.AWVdrvdp;
+        /// xs=107%3Ahlw-ZENUDzjacA%3A2%3A1407611980%3A8886;
+        /// csm=2;
+        /// s=Aa7ySACnrs6HVvqp
+        /// </summary>
+		public void AddDefaultHeaders()
+		{
+			Headers.Add("Accept", "text/html, application/xhtml+xml, */*");
+			Headers.Add("Accept-Language", "en-gb");
+            Headers.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.3; WOW64; Trident/7.0; MAARJS)");
+			Headers.Add("Cache-Control", "no-cache");
+			//Headers.Add("X-SVN-Rev", "570609");
+			//Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+			//Headers.Add("Accept-Encoding", "gzip, deflate");
+		}
+
+		private void AddCookies(HttpWebRequest wr)
+		{
+			string sPath = "/";
+			string sDomain = "www.facebook.com";
+			wr.CookieContainer = new CookieContainer();
+			CookieContainer aCC = wr.CookieContainer;
+
+			try
+			{
+				Cookie c = new Cookie("presence", "EM338654546EuserFA2659625177A2EstateFDsb2F0Et2F_5b_5dElm2FnullEuct2F1338651218BEtrFA2loadA2EtwF240618245EatF1338654185986G338654546498EsndF0EnotF0CEchFDp_5f659625177F31CC", sPath, sDomain);
+				aCC.Add(c);
+				//p=3; presence=; act=; datr=; locale=en_US; lu=; c_user=; csm=2; s=; xs=;
+				c = new Cookie("sub", "1", sPath, sDomain); aCC.Add(c); //ChangedNextTime - CNT
+				c = new Cookie("p", "3", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("act", "1339186818432%2F7%3A2", sPath, sDomain); aCC.Add(c); //CNT
+				c = new Cookie("datr", "tlPDTzRmC25sWkXn4UHYAaBY", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("locale", "en_US", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("lu", "gAXoLw-DCrnHS9MTZh5-F4Ag", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("c_user", "659625177", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("csm", "2", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("s", "Aa5Eeeg4qdtbj9mr", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("xs", "3%3ABM89Z2TAic8JZg%3A2%3A1338651769", sPath, sDomain); aCC.Add(c);
+				c = new Cookie("wd", "1366x654", sPath, sDomain); aCC.Add(c); //CNT
+			}
+			catch (Exception ex)
+			{
+				ex.ToString();
+			}
+		}
+	}
+}
---
+++ DeleteFBActivities/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+using System.Net;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+
+namespace DeleteFBActivities
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+
+				CookieAwareWebClient wc = new CookieAwareWebClient();
+				wc.AddDefaultHeaders();
+                byte[] buff = wc.DownloadData("https://www.facebook.com/the.habib.qureshi/allactivity"); //https://www.facebook.com/all_activity.php - 2012
+
+				DateTime dt = DateTime.Now; //DateTime dt = DateTime.ParseExact("6/10/2012", "M/d/yyyy", null);
+				String sDate = dt.ToString("M/d/yyyy");
+				sDate = Uri.EscapeDataString(sDate);
+
+				int main_count = 0;
+				while(true)
+				{
+					main_count++;
+					int i = 0;
+					System.Console.WriteLine(dt.ToShortDateString());
+					while (DeleteActivityOnThisDate(wc, sDate, dt)) i++;
+					System.Threading.Thread.Sleep(500);
+					dt = dt.AddDays(-1);
+					sDate = dt.ToString("M/d/yyyy");
+					sDate = Uri.EscapeDataString(sDate);
+					if (main_count >= 5000) break; //10 years?
+				}
+			}
+			catch (Exception ex)
+			{
+				ex.ToString();
+			}
+		}
+
+		public static bool DeleteActivityOnThisDate(WebClient wc, string this_date, DateTime dt)
+		{
+			int nWebExceptionCount = 0;
+		RESTART_WORK:
+			nWebExceptionCount++;
+			try
+			{
+				byte[] buff = wc.DownloadData("https://www.facebook.com/ajax/pagelet/generic.php/FBXLogStoriesPagelet?__a=1&data=%7B%22date_str%22%3A%22" + this_date + "%22%2C%22log_filter%22%3Anull%2C%22profile_id%22%3A659625177%7D&__user=659625177");
+
+				string sHtmlResult = System.Text.Encoding.UTF8.GetString(buff);
+				int nIndex = sHtmlResult.IndexOf("No activity could be found");
+				if (nIndex != -1) return false;
+
+				string story_div_id = "story_div_id";
+				string story_dom_id = "story_dom_id";
+				string story_fbid = "story_fbid";
+				string story_row_time = "story_row_time";
+				story_div_id = sHtmlResult.Substring(sHtmlResult.IndexOf(story_div_id) + story_div_id.Length + 1, 8);
+				story_dom_id = sHtmlResult.Substring(sHtmlResult.IndexOf(story_dom_id) + story_dom_id.Length + 1, 8);
+				int start = sHtmlResult.IndexOf(story_fbid) + story_fbid.Length + 1;
+				int end = sHtmlResult.IndexOf("&amp;", start);
+				story_fbid = sHtmlResult.Substring(start, end-start);
+				story_row_time = sHtmlResult.Substring(sHtmlResult.IndexOf(story_row_time) + story_row_time.Length + 1, 10);
+
+				//Now prepare to delete all activities, 1 by 1
+				wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+				string sURI = "https://www.facebook.com/ajax/timeline/take_action_on_story.php?__a=1";
+				string myParamters = "permalink=1&profile_id=659625177&story_div_id={0}&story_dom_id={1}&story_fbid={2}&story_row_time={3}&__a=1&__user=659625177&action=remove_content&also_remove_app=0&fb_dtsg=AQCgOY5i&phstamp=1658167103798953105211";
+				myParamters = String.Format(myParamters, story_div_id, story_dom_id, story_fbid, story_row_time);
+				ServicePointManager.Expect100Continue = false;
+				sHtmlResult = wc.UploadString(sURI, "POST", myParamters); //requesting to delete the particular activity
+				myParamters += "&confirmed=true&ban_user=0";
+				sHtmlResult = wc.UploadString(sURI, "POST", myParamters); //confirmation to delete the activity.
+
+				nIndex = sHtmlResult.IndexOf("No Permission to Post");
+				if (nIndex != -1)
+				{
+					using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\FB.txt", true))
+					{
+						file.WriteLine(dt.ToShortDateString()); //From here you remove these activities manually.
+					}
+					return false;
+				}
+
+				return true;
+			}
+			catch (WebException wex)
+			{
+				wex.ToString();
+				if(nWebExceptionCount < 3)
+					goto RESTART_WORK;
+				using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\FB.txt", true))
+				{
+					file.WriteLine(dt.ToShortDateString()); //From here you remove these activities manually.
+				}
+				return false;
+			}
+			catch (Exception)
+			{
+				using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\FB.txt", true))
+				{
+					file.WriteLine(dt.ToShortDateString()); //From here you remove these activities manually.
+				}
+				return false;
+			}
+		}
+	}
+}
